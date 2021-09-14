86c4016e64a8a6c58d736b4787fe4d8d72dd6f8d
Marco Fuentes <marqo.fe@gmail.com>
2021-09-13 21:27:58 -0500

    Agregue los archivos necesarios para la actualizacion del perfil y el middleware

---
+++ foodgram/foodgram/middleware.py
@@ -0,0 +1,27 @@
+"""Foodgram middleware catalog"""
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class ProfileCompletionMiddleware:
+    """Profile completion middleware.
+
+        Ensure every user that is interacting that with the
+        platform to have a profile picture
+    """
+
+    def __init__(self, get_response):
+        """Middleware initialization."""
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """Code to be executed for each request"""
+        print("Redirected to middleware")
+        if not request.user.is_anonymous:
+            if not request.user.is_staff:
+                profile = request.user.profile
+                if not profile.picture or not profile.biography:
+                    if request.path not in [reverse('update_profile'), reverse('logout')]:
+                        return redirect('update_profile')
+
+        response = self.get_response(request)
+        return response
